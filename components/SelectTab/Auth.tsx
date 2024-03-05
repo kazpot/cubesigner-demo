b4cb24bddace1f5885cafa09688a751f7a3611a1
kaz <knman7@hotmail.com>
2024-03-06 00:19:22 +0900

    Add google oauth2

--- components/SelectTab/Auth.tsx
+++ components/SelectTab/Auth.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import styles from "./Auth.module.css";
+import axios from "axios";
+import { generators } from "openid-client";
 
 type Props = {
   currentAccount: string | undefined;
@@ -6,11 +9,32 @@ type Props = {
 };
 
 export default function Auth({ currentAccount, connectWallet }: Props) {
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID || "";
+  const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI || "";
+  const oauth2Endpoint = process.env.NEXT_PUBLIC_OAUTH2_ENDPOINT || "";
+
+  const handleSubmit = () => {
+    const nonce = generators.nonce();
+    const params = {
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      response_type: "code id_token",
+      scope: "https://mail.google.com/",
+      include_granted_scopes: "true",
+      state: "pass-through value",
+      nonce,
+    };
+
+    const url = new URL(oauth2Endpoint);
+    url.search = new URLSearchParams(params).toString();
+    window.open(url.toString(), "_self");
+  };
+
   return (
     <div className={styles.authBody}>
       {currentAccount === undefined ? (
         <div className={styles.bottomDiv}>
-          <div className={styles.btn} onClick={() => ""}>
+          <div className={styles.btn} onClick={handleSubmit}>
             Google
           </div>
         </div>
